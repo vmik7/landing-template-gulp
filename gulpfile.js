aedfc03c61b52d6f7bd1dc0f79cc25ab95bd926a
vmik7 <vmik7@yandex.ru>
2021-09-09 13:12:00 +0300

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,40 +2,44 @@
 
 require('dotenv').config();
 
+/* node packages */
 const fsp = require('fs/promises');
 const path = require('path');
 
+/* gulp */
 const { src, dest, watch, series, parallel } = require('gulp');
+
+/* utils */
 const del = require('del');
 const browserSync = require('browser-sync').create();
 const rename = require('gulp-rename');
 const fileInclude = require('gulp-file-include');
 
-// works with CSS
+/* works with CSS */
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 const groupMedia = require('gulp-group-css-media-queries');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
 
-// works with JS
+/* works with JS */
 const uglify = require('gulp-uglify-es').default;
 
-// works with Images
+/* works with images */
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 
-// works with Fonts
+/* works with fonts */
 const ttf2woff = require('ttf2woff');
 const ttf2woff2 = require('ttf2woff2');
 
-// works with deploy
+/* deploy */
 const ghpages = require('gh-pages');
 
-// routing
+/* routing */
 const routes = require('./gulp/routes');
 
-// BrowserSync configuration
+/* BrowserSync configuration */
 function browserSyncConfig() {
     browserSync.init({
         server: {
@@ -47,7 +51,7 @@ function browserSyncConfig() {
     });
 }
 
-// * HTML
+/* HTML */
 
 function html() {
     return (
@@ -59,7 +63,7 @@ function html() {
     );
 }
 
-// * CSS
+/* CSS */
 
 function css() {
     return src(routes.src.scss)
@@ -86,7 +90,7 @@ function css() {
         .pipe(browserSync.stream());
 }
 
-// * JS
+/* JS */
 
 function js() {
     return (
@@ -104,7 +108,7 @@ function js() {
     );
 }
 
-// * Images
+/* IMAGES */
 
 function images() {
     return src(routes.src.images)
@@ -127,13 +131,13 @@ function images() {
         .pipe(browserSync.stream());
 }
 
-// * Fonts
+/* FONTS */
 
 function fonts() {
     return src(routes.src.fonts).pipe(dest(routes.build.fonts));
 }
 
-// Converting .ttf fonts to .woff and .woff2
+/* Converting .ttf fonts to .woff and .woff2 */
 async function convertFonts() {
     await fsp.mkdir(path.resolve(routes.prepare.fontsTo), {
         recursive: true,
@@ -170,7 +174,7 @@ async function convertFonts() {
     }
 }
 
-// imports builded Fonts
+/* imports builded Fonts */
 async function importFonts() {
     const fontsFileName = path.resolve(
         routes.srcFolderName,
@@ -191,12 +195,13 @@ async function importFonts() {
     }
 }
 
+/* convert and import ttf fonts */
 async function prepareFonts() {
     await convertFonts();
     await importFonts();
 }
 
-// Monitors source failes to be changed
+/* Monitors source failes to be changed */
 function watchFiles() {
     watch([routes.watch.html], html);
     watch([routes.watch.css], css);
@@ -205,12 +210,12 @@ function watchFiles() {
     watch([routes.watch.fonts], fonts);
 }
 
-// Clean build directory
+/* Clean build directory */
 function clean() {
     return del(routes.buildFolderName);
 }
 
-// Deploy build to the gh-pages branch
+/* Deploy build to the gh-pages branch */
 async function deploy() {
     await ghpages.publish(routes.buildFolderName, {}, (res) => {
         if (res.output) console.log(res.output);
